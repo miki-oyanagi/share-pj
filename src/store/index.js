a569b154bfefbef6298a5de228b4414a83755bbe
miki oyanagi <mikioyanagi.0806@icloud.com>
2021-05-15 17:31:13 +0900

    0515third

--- src/store/index.js
+++ src/store/index.js
@@ -29,14 +29,14 @@ export default new Vuex.Store({
   actions: {
     async login({commit},{email,password}){
       const responseLogin=await axios.post(
-        "https://guarded-lake-44917.herokuapp.com/api/login",
+        "https://enigmatic-refuge-85810.herokuapp.com/api/login",
         {
           email:email,
           password:password,
         }
       );
       const responseUser =await axios.get(
-        "https://guarded-lake-44917.herokuapp.com/api/user",
+        "https://enigmatic-refuge-85810.herokuapp.com/api/user",
         {
           params:{
             email:email,
@@ -49,7 +49,7 @@ export default new Vuex.Store({
     },
     logout({commit}){
       axios
-      .post("https://guarded-lake-44917.herokuapp.com/api/logout",{
+      .post("https://enigmatic-refuge-85810.herokuapp.com/api/logout",{
         auth:this.state.auth,
       })
       .then((response)=>{
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -38,7 +38,7 @@ export default {
     edit(){
       if(!this.active){
         axios
-        .put("https://guarded-lake-44917.herokuapp.com/api/user",{
+        .put("https://enigmatic-refuge-85810.herokuapp.com/api/user",{
           email:this.$store.state.user.email,
           profile:this.profile,
         })
